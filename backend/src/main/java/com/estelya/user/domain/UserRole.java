9c13eca0c68d81a04354eee7b754f54c2fc59cc0
Francinei Oliveira <fgoliveira2@outlook.com>
2026-08-23 00:17:44 -0400

    feat: add user domain model

---
+++ backend/src/main/java/com/estelya/user/domain/UserRole.java
@@ -0,0 +1,7 @@
+package com.estelya.user.domain;
+
+public enum UserRole {
+    ADMIN,
+    MANAGER,
+    OPERATOR
+}
---
+++ backend/src/main/java/com/estelya/user/domain/UserStatus.java
@@ -0,0 +1,6 @@
+package com.estelya.user.domain;
+
+public enum UserStatus {
+    ACTIVE,
+    INACTIVE
+}
